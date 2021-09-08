7d7bbdca54ec108341841309cac4d1eda2912a40
Dohyeong Kim <o2kim0225@naver.com>
2021-09-08 23:12:45 +0900

    install firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA1iEw7MsrhJfrjtgv2J3SbtaIvKUxQrRw",
+    authDomain: "nwitter-5f6dc.firebaseapp.com",
+    projectId: "nwitter-5f6dc",
+    storageBucket: "nwitter-5f6dc.appspot.com",
+    messagingSenderId: "156566718182",
+    appId: "1:156566718182:web:5f350b02e73cc461179d27"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
