3ac6c4c4dc8c8b45cd97c4cb6b08a35ced0c9573
Dohyeong Kim <o2kim0225@naver.com>
2021-10-10 18:08:03 +0900

    impl - 트윗 삭제 기능

---
+++ src/components/Nweet.js
@@ -0,0 +1,24 @@
+import { dbService } from "fbase";
+
+const Nweet = ({ nweetObj, isOwner }) => {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("삭제하시겠습니까?");
+    if (ok) {
+      const data = await dbService.doc(`nweets/${nweetObj.id}`).delete();
+    }
+  };
+
+  return (
+    <div>
+      <h4>{nweetObj.text}</h4>
+      {isOwner && (
+        <>
+          <button onClick={onDeleteClick}>Delete Nweet</button>
+          <button>Edit Nweet</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Nweet;
