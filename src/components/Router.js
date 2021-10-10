fb37e4c76649a62f343ad474b59403a1c30eea6c
Dohyeong Kim <o2kim0225@naver.com>
2021-10-10 17:18:17 +0900

    impl - save uid

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,30 +4,29 @@ import Home from "routes/Home";
 import Profile from "routes/Profile";
 import Navigation from "./Navigation";
 
-const AppRouter = ({ isLoggedIn }) => {
-    return (
-        <Router>
-            {isLoggedIn && <Navigation />}
-            <Switch>
-                {isLoggedIn ? (
-                    // NOTE : JSX에서는 하나로 묶어야 정상적으로 작동함 <> ... </>
-                    <>
-                        <Route exact path="/">
-                            <Home />
-                        </Route>
-                        <Route exact path="/profile">
-                            <Profile />
-                        </Route>
-                    </>
-                ) : (
-                    <Route exact path="/">
-                        <Auth />
-                    </Route>
-                )
-                }
-            </Switch>
-        </Router>
-    );
+const AppRouter = ({ isLoggedIn, userObj }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Switch>
+        {isLoggedIn ? (
+          // NOTE : JSX에서는 하나로 묶어야 정상적으로 작동함 <> ... </>
+          <>
+            <Route exact path="/">
+              <Home userObj={userObj} />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
 };
 
 export default AppRouter;
